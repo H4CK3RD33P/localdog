4f3c93e4fd30497aeaa4efef8f21b36996198c16
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2022-04-24 23:37:07 +0530

    Added support for handling invalid inputm using regex

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from mac_vendor_lookup import MacLookup
 from scapy.all import Ether, ARP, srp
 import sys
+import re
 
 print("Updating Vendors...")
 macobj = MacLookup()
 macobj.update_vendors()
 
 iprange = input("Enter your private IP (eg. 100.10.1.1/24): ")
+if not bool(re.match(r"\d{,3}\.\d{,3}\.\d{,3}\.\d{,3}/\d{1,2}",iprange)):
+    print("Invalid IP")
+    sys.exit()
+
 try:
     arp = ARP(pdst=iprange)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
